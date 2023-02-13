871e6ec8e2cb337fb4baa67ff4907fb6f696100f
DamianPiechota <frisky16@gmail.com>
2023-02-13 22:54:29 +0100

    update js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const liEls = [];
+const itemEl = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  liEls.push(liEl);
+}
+
+itemEl.append(...liEls);
